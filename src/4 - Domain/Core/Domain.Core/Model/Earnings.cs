936be2d8b48250ba7ce6d93e958e6e86cbade989
ejaassuncao <ewa.soft@gmail.com>
2023-01-01 03:03:26 -0300

    cadastro de Proventos

---
+++ src/4 - Domain/Core/Domain.Core/Model/Earnings.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Commons.Entity;
+using Domain.Core.Model.Actives;
+using System;
+
+namespace Domain.Core.Model
+{
+    /// <summary>
+    /// Classe de gerencimento de proventos
+    /// </summary>
+    public class Earnings : EntityBase
+    {
+        public AbstractActives Active { get; init; }
+        public string Type { get; init; } //dividendos/juros sobre capital, Bonificação desdobramento, Redimentos
+        public DateTime Date { get; init; }
+
+
+        public Earnings(AbstractActives active, string type, DateTime date)
+        {
+            Active = active;
+            Type = type;
+            Date = date;
+        }
+
+        public Earnings(int id, AbstractActives active, string type, DateTime date) : base(id)
+        { 
+            Active = active;
+            Type = type;
+            Date = date;
+        }
+    }
+}
